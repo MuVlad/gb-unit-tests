aa3a66c6e693e96b678be9f8c2fe8b4204e1f87c
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-09-14 20:33:23 +0500

    подготовка к дз2

---
+++ src/test/java/hw2/VehicleTest.java
@@ -0,0 +1,13 @@
+package hw2;
+
+import org.example.hw2.Car;
+import org.example.hw2.Motorcycle;
+import org.example.hw2.Vehicle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+}
